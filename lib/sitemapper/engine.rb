4d0eb0b37eec57141c0bac02359f6c169d765acd
Carsten Zimmermann <carp@hacksocke.de>
2013-09-28 22:14:48 +0200

    Add ActiveRecord methods in engine initializer

--- lib/sitemapper/engine.rb
+++ lib/sitemapper/engine.rb
@@ -10,6 +10,12 @@ module AegisNet
         AegisNet::Sitemapper.sitemap_file ||= File.join(app.root, "config", "sitemaps.yml")
       end
 
+      initializer 'ar_sitemapper.hook_into_active_record' do
+        ActiveSupport.on_load(:active_record) do
+          ::ActiveRecord::Base.send :include, AegisNet::Sitemapper::ActiveRecord::Builder
+        end
+      end
+
     end
 
   end
--- test/unit/engine_test.rb
+++ test/unit/engine_test.rb
@@ -6,4 +6,14 @@ describe AegisNet::Sitemapper::Engine do
     AegisNet::Sitemapper.sitemap_file.must_equal "#{Rails.root}/config/sitemaps.yml"
   end
 
+  it 'integrates with ActiveRecord' do
+    active_record_builder::SingletonMethods.instance_methods.each do |method|
+      ActiveRecord::Base.must_respond_to method
+    end
+  end
+
+  def active_record_builder
+    AegisNet::Sitemapper::ActiveRecord::Builder
+  end
+
 end
