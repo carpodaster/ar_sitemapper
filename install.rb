a00ac1a6fa719b202ec8840a6612bba4e2b2e301
Carsten Zimmermann <carp@hacksocke.de>
2013-09-29 00:15:15 +0200

    Remove plugin artifact

--- install.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'config/environment'
-FileUtils.cp File.join(File.dirname(__FILE__), "templates", "sitemapper.rake"), File.join(Rails.root, "tasks")
-FileUtils.cp File.join(File.dirname(__FILE__), "templates", "sitemaps.yml"), File.join(Rails.root, "config")
