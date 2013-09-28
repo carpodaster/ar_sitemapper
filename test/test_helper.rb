8edb704d031d2a51e6d167143e652bdde55ff9d1
Carsten Zimmermann <carp@hacksocke.de>
2013-09-28 21:41:14 +0200

    Use bundler to load stuff

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,10 +4,9 @@ require File.expand_path('../dummy/config/environment.rb', __FILE__)
 
 require 'test/unit'
 require 'rubygems'
-require 'mocha'
-require 'active_support/core_ext'
+require 'bundler'
+Bundler.require(:default, :development)
 
 require 'erb'
 
-require 'lib/ar_sitemapper'
 Dir.glob(File.join(File.dirname(__FILE__) + 'support', 'app', '*') {|file| require file})
